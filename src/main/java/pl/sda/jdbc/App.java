bfd9c7eadf38a8b1ee0c7698bcc884cf7f0d153a
Mateusz Burdelak <mateusz.burdelak@o2.pl>
2019-08-10 11:29:03 +0200

    initial repo

---
+++ src/main/java/pl/sda/jdbc/App.java
@@ -0,0 +1,14 @@
+package pl.sda.jdbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
